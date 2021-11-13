51260dd3b266706d56b769b0665c04e1bba98848
Prantosh <bepariprantosh360@gmail.com>
2021-11-13 13:11:17 +0600

    honda show-room sarver

---
+++ index.js
@@ -0,0 +1,207 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const admin = require("firebase-admin"); //
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 5000;
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); //
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount), //
+});
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.vh8a9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function verifyToken(req, res, next) {
+  if (req.headers?.authorization?.startsWith("Bearer ")) {
+    const token = req.headers.authorization.split(" ")[1];
+
+    try {
+      const decodedUser = await admin.auth().verifyIdToken(token);
+      req.dicodedEmail = decodedUser.email;
+    } catch {}
+  }
+  next();
+}
+
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("honda-show-room");
+    const ProductsCollection = database.collection("products");
+    const usersCollection = database.collection("users");
+    const ordersCollection = database.collection("orders");
+    const reviewCollection = database.collection("reviews");
+
+    // get all reviews
+    app.get("/reviews", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get single reviews
+    app.get("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.findOne(query);
+      res.json(result);
+    });
+
+    // get all products
+    app.get("/products", async (req, res) => {
+      const cursor = ProductsCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // get all orders
+    app.get("/allOrders", async (req, res) => {
+      const cursor = ordersCollection.find({});
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    // delete all orders
+    app.delete("/allOrders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // delete products
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ProductsCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // get single products
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ProductsCollection.findOne(query);
+      res.json(result);
+    });
+
+    // add products
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await ProductsCollection.insertOne(product);
+      res.json(result);
+    });
+
+    // add orders my order
+    app.get("/orders", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = await ordersCollection.find(query).toArray();
+      res.json(cursor);
+    });
+
+    // delete my orders
+    app.delete("/orders/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await ordersCollection.deleteOne(query);
+      res.json(result);
+    });
+
+    // add order
+    app.post("/orders", async (req, res) => {
+      const order = req.body;
+      order.createdAt = new Date();
+      const result = await ordersCollection.insertOne(order);
+      res.json(result);
+    });
+
+    // single data update
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await ProductsCollection.insertOne(product);
+      res.json(result);
+    });
+
+    // varify admin for databasepoint
+    app.get("/users/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      let isAdmin = false;
+      if (user?.role === "admin") {
+        isAdmin = true;
+      }
+      res.json({ admin: isAdmin });
+    });
+
+    // set the database single user email an name
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+      res.json(result);
+    });
+
+    // google email update database
+    app.put("/users", async (req, res) => {
+      const user = req.body;
+      const filter = { emil: user.emil };
+      const options = { upsert: true };
+      const updateDoc = { $set: user };
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
+      res.json(result);
+    });
+
+    // add user admin
+    app.put("/users/admin", verifyToken, async (req, res) => {
+      const user = req.body;
+      const requester = req.dicodedEmail;
+      if (requester) {
+        const requesterAccount = await usersCollection.findOne({
+          email: requester,
+        });
+        if (requesterAccount.role === "admin") {
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: "admin" } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
+        }
+      } else {
+        res
+          .status(403)
+          .json({ message: "you do not have an access to make admin" });
+      }
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Welcome Honda show-room");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
